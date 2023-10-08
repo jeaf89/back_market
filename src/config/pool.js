6b8e5fabaad6808541873454e4b333ca67fa1eee
jeaf89 <ing.jeaf.89@gmail.com>
2023-10-08 20:12:49 -0300

    ssl

--- src/config/pool.js
+++ src/config/pool.js
@@ -3,21 +3,23 @@ const { Pool } = require('pg');
 //const connectionString = process.env.CONNECTION_STRING;
 
 const pool = new Pool({
-     user: "admin",
-     host: "dpg-ckfjocuct0pc73bld730-a.oregon-postgres.render.com",
-     database: "art_market",
-     password: "48sW9XX6lLQrlIOSfw8BT92SwvTG76i1",
+      user: "admin",
+      host: "dpg-ckfjocuct0pc73bld730-a.oregon-postgres.render.com",
+      database: "art_market",
+      password: "48sW9XX6lLQrlIOSfw8BT92SwvTG76i1",
+      allowExitOnIdle: true,
+      ssl: true
+});
+
+/*
+const pool = new Pool({
+     user: "postgres",
+     host: "localhost",
+     database: "artMarket",
+     password: "jeaf89",
      allowExitOnIdle: true
  });
-
-// const pool = new Pool({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "artMarket",
-//     password: "jeaf89",
-//     allowExitOnIdle: true
-// });
-
+/*
 /*const pool = new Pool({
     connectionString,
 });*/
